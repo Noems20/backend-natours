3ad54ed56f9924b314ee04b00477e472a690238a
Noé Muñoz Sánchez <noesm.16@gmail.com>
2021-08-30 12:39:38 -0500

    Created update and delete controllers for logged user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,8 @@ import {
   createUser,
   updateUser,
   deleteUser,
+  updateMe,
+  deleteMe,
 } from '../controllers/userController.js';
 
 import {
@@ -25,6 +27,8 @@ router.post('/login', login);
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
 router.patch('/updateMyPassword', protect, updatePassword);
+router.patch('/updateMe', protect, updateMe);
+router.delete('/deleteMe', protect, deleteMe);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
