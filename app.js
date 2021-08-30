f72b08dedf7e1148f46e08a9046c43841aa81a57
Noé Muñoz Sánchez <noesm.16@gmail.com>
2021-08-30 16:03:59 -0500

    Finished security and authorization adding middlewares for Xss, params pollution, rate limit and data sanitization

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import colors from 'colors';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean';
+import hpp from 'hpp';
 
 // Utils
 import AppError from './utils/appError.js';
@@ -14,7 +19,47 @@ import userRouter from './routes/userRoutes.js';
 const app = express();
 
 // Middleware
-app.use(express.json());
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit request from API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading from body into req.body
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Serving static fies
+// app.use(express.static(`${__dirname}/public`))
 
 // Routes
 app.use('/api/v1/tours', tourRouter);
